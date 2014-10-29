7aded37e6b3b07de3e8a7dd92f1254e3835d9436
rgnazar <git@astronomoamador.com.br>
2014-10-29 16:38:37 -0300

    Telas de Status Atualizada

--- src/br/com/astronomoamador/firgoto/StatusActivity.java
+++ src/br/com/astronomoamador/firgoto/StatusActivity.java
@@ -10,7 +10,9 @@ public class StatusActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_status);
+		
 	}
 
 	@Override
