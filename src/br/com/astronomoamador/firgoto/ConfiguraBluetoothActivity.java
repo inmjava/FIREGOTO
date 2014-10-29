de9fd0723c45783bf7f6c6eac3f770f1927b6569
C050782 <C050782@P017818.copel.nt>
2014-10-29 13:01:59 -0300

    Criação da tela de configuração de Bluetooth

--- src/br/com/astronomoamador/firgoto/ConfiguraBluetoothActivity.java
+++ src/br/com/astronomoamador/firgoto/ConfiguraBluetoothActivity.java
@@ -1,24 +1,22 @@
 package br.com.astronomoamador.firgoto;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends Activity {
+public class ConfiguraBluetoothActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_configura_bluetooth);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.configura_bluetooth, menu);
 		return true;
 	}
 
@@ -33,9 +31,4 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public void configurarBluetooth(View v){
-		startActivity(new Intent(this, ConfiguraBluetoothActivity.class));
-	}
-	
 }
