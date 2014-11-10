2d5d5e146683a4b6cd347684e31b9ba95654c8fe
rgnazar <git@astronomoamador.com.br>
2014-11-09 23:22:06 -0300

    mais cmd

--- src/br/com/astronomoamador/firgoto/StatusActivity.java
+++ src/br/com/astronomoamador/firgoto/StatusActivity.java
@@ -13,6 +13,7 @@ import android.os.Message;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,7 +21,34 @@ public class StatusActivity extends Activity {
 
 	private BluetoothDevice mmDevice;
 	private ConnectedThread connectedThread;
+
 	private EditText editTextGrauLat;
+	private EditText editTextMinLat;
+	private EditText editTextSegLat;
+
+	private EditText editTextGrauLog;
+	private EditText editTextMinLog;
+	private EditText editTextSegLog;
+
+	private EditText editTextHoraTime;
+	private EditText editTextMinTime;
+	private EditText editTextSegTime;
+
+	private EditText editTextUTCSet;
+
+	private EditText editTextHoraUTC;
+	private EditText editTextMinUTC;
+	private EditText editTextSegUTC;
+
+	private EditText editTextHoraLST;
+	private EditText editTextMinLST;
+	private EditText editTextSegLST;
+
+
+	private String bufferCmd = "";
+	private String commandAtual = null;
+
+
 	private final Handler mHandler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
@@ -29,7 +57,18 @@ public class StatusActivity extends Activity {
 				byte[] readBuf = (byte[]) msg.obj;
 				// construct a string from the valid bytes in the buffer
 				String readMessage = new String(readBuf, 0, msg.arg1);
-				editTextGrauLat.setText(editTextGrauLat.getText().toString() + readMessage);
+				if (!readMessage.contains("#"))
+				{
+					bufferCmd=bufferCmd+readMessage;	
+				}
+				else
+				{
+					bufferCmd=bufferCmd+readMessage;
+					leResposta(bufferCmd);
+					bufferCmd="";
+				}
+				readBuf=null;
+				readMessage=null;
 				break;
 			}
 		}
@@ -39,18 +78,71 @@ public class StatusActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
+
 		editTextGrauLat = (EditText) findViewById(R.id.editTextGrauLat);
+		editTextMinLat = (EditText) findViewById(R.id.editTextMinLat);
+		editTextSegLat = (EditText) findViewById(R.id.editTextSegLat);
+
+		editTextGrauLog = (EditText) findViewById(R.id.editTextGrauLog);
+		editTextMinLog = (EditText) findViewById(R.id.editTextMinLog);
+		editTextSegLog = (EditText) findViewById(R.id.editTextSegLog);
+
+		editTextHoraTime = (EditText) findViewById(R.id.editTextHoraTime);
+		editTextMinTime = (EditText) findViewById(R.id.editTextMinTime);
+		editTextSegTime = (EditText) findViewById(R.id.editTextSegTime);
+
+		editTextUTCSet = (EditText) findViewById(R.id.editTextUTCSet);
+
+		editTextHoraUTC = (EditText) findViewById(R.id.editTextHoraUTC);
+		editTextMinUTC = (EditText) findViewById(R.id.editTextMinUTC);
+		editTextSegUTC = (EditText) findViewById(R.id.editTextSegUTC);
+
+		editTextHoraLST = (EditText) findViewById(R.id.editTextHoraLST);
+		editTextMinLST = (EditText) findViewById(R.id.editTextMinLST);
+		editTextSegLST = (EditText) findViewById(R.id.editTextSegLST);
+
+
+
+
+
+
 		mmDevice = getIntent().getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 		if(mmDevice != null){
 			try {
 				connectedThread = new ConnectedThread(mmDevice, mHandler);
 				new Thread(connectedThread).start();
-				connectedThread.write("statusActivity".getBytes());
+				mandacommand();
 			} catch (IOException e) {
 				Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
 				//					e.printStackTrace();
 			}
 		}
+	}
+
+	void mandacommand()
+	{ 
+		try {
+			commandAtual=":Gt#";
+			connectedThread.write(commandAtual.getBytes());
+			Thread.sleep(250);
+			commandAtual=":Gg#";
+			connectedThread.write(commandAtual.getBytes());
+			Thread.sleep(250);
+			commandAtual=":GG#";
+			connectedThread.write(commandAtual.getBytes());
+			Thread.sleep(250);
+			commandAtual=":GL#";
+			connectedThread.write(commandAtual.getBytes());
+			Thread.sleep(250);
+			commandAtual=":GS#";
+			connectedThread.write(commandAtual.getBytes());
+			Thread.sleep(250);
+
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -89,4 +181,44 @@ public class StatusActivity extends Activity {
 		}
 		return super.onKeyDown(keyCode, event);
 	}	
+
+
+	private void leResposta(String readMessage)
+	{
+		if (":Gt#".equals(commandAtual)) {
+		editTextGrauLat.setText(readMessage.subSequence(0, 2));
+		editTextMinLat.setText(readMessage.subSequence(3, 5));
+		editTextSegLat.setText(readMessage.subSequence(6, 8));
+		}
+		if (":Gg#".equals(commandAtual)) {
+		editTextGrauLog.setText(readMessage.subSequence(0, 2));
+		editTextMinLog.setText(readMessage.subSequence(3, 5));
+		editTextSegLog.setText(readMessage.subSequence(6, 8));
+		}
+		if (":GLGL#".equals(commandAtual)) {
+		editTextHoraTime.setText(readMessage.subSequence(0, 2));
+		editTextMinTime.setText(readMessage.subSequence(3, 5));
+		editTextSegTime.setText(readMessage.subSequence(6, 8));
+		}
+		if (":GG#".equals(commandAtual)) {
+		editTextUTCSet.setText(readMessage.subSequence(0, 2));
+		}
+		if (":GL#".equals(commandAtual)) {
+		editTextHoraUTC.setText(readMessage.subSequence(0, 2));
+		editTextMinUTC.setText(readMessage.subSequence(3, 5));
+		editTextSegUTC.setText(readMessage.subSequence(6, 8));
+		}
+		if (":GS#".equals(commandAtual)) {
+		editTextHoraLST.setText(readMessage.subSequence(0, 2));
+		editTextMinLST.setText(readMessage.subSequence(3, 5));
+		editTextSegLST.setText(readMessage.subSequence(6, 8));
+		}
+
+	}
+
+	public void atualizar(View v){
+		mandacommand();
+	}
+	
+
 }
--- src/br/com/astronomoamador/firgoto/bluetooth/ConnectedThread.java
+++ src/br/com/astronomoamador/firgoto/bluetooth/ConnectedThread.java
@@ -4,70 +4,73 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.UUID;
-
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Handler;
 
 public class ConnectedThread extends Thread {
-    private BluetoothSocket mmSocket;
-    private InputStream mmInStream;
-    private OutputStream mmOutStream;
+	private BluetoothSocket mmSocket;
+	private InputStream mmInStream;
+	private OutputStream mmOutStream;
 	private Handler mHandler;
 	private boolean finished = false;
- 
-    public ConnectedThread(BluetoothDevice device, Handler handler) throws IOException {
-    	UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // Standard
-    	
-    	mmSocket = device.createRfcommSocketToServiceRecord(uuid);
+	private long time = System.currentTimeMillis(); 
+	public ConnectedThread(BluetoothDevice device, Handler handler) throws IOException {
+		UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // Standard
+
+		mmSocket = device.createRfcommSocketToServiceRecord(uuid);
 		mmSocket.connect();
-    	
-        InputStream tmpIn = null;
-        OutputStream tmpOut = null;
- 
-        // Get the input and output streams, using temp objects because
-        // member streams are final
-        try {
-            tmpIn = mmSocket.getInputStream();
-            tmpOut = mmSocket.getOutputStream();
-        } catch (IOException e) { }
- 
-        mmInStream = tmpIn;
-        mmOutStream = tmpOut;
-        mHandler = handler;
-    }
- 
-    public void run() {
-        byte[] buffer = new byte[1024];  // buffer store for the stream
-        int bytes; // bytes returned from read()
- 
-        // Keep listening to the InputStream until an exception occurs
-        while (!isFinished()) {
-            try {
-                // Read from the InputStream
-                bytes = mmInStream.read(buffer);
-                // Send the obtained bytes to the UI activity
-                mHandler.obtainMessage(Constants.MESSAGE_READ, bytes, -1, buffer).sendToTarget();
-            } catch (IOException e) {
-                break;
-            }
-        }
-    }
- 
-    /* Call this from the main activity to send data to the remote device */
-    public void write(byte[] bytes) {
-        try {
-            mmOutStream.write(bytes);
-        } catch (IOException e) { }
-    }
- 
-    /* Call this from the main activity to shutdown the connection */
-    public void finish() {
-        try {
-            mmSocket.close();
-            setFinished(true);
-        } catch (IOException e) { }
-    }
+
+		InputStream tmpIn = null;
+		OutputStream tmpOut = null;
+
+		// Get the input and output streams, using temp objects because
+		// member streams are final
+		try {
+			tmpIn = mmSocket.getInputStream();
+			tmpOut = mmSocket.getOutputStream();
+		} catch (IOException e) { }
+
+		mmInStream = tmpIn;
+		mmOutStream = tmpOut;
+		mHandler = handler;
+	}
+
+	public void run() {
+		byte[] buffer = new byte[1024];  // buffer store for the stream
+		int bytes; // bytes returned from read()
+
+		// Keep listening to the InputStream until an exception occurs
+		while (!isFinished()) {
+			try {
+				// Read from the InputStream
+				bytes = mmInStream.read(buffer);
+				// Send the obtained bytes to the UI activity
+				mHandler.obtainMessage(Constants.MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+				time = System.currentTimeMillis()+50;
+				while (time > System.currentTimeMillis())
+				{
+				}
+			} catch (IOException e) {
+				break;
+			}
+		}
+	}
+
+	/* Call this from the main activity to send data to the remote device */
+	public void write(byte[] bytes) {
+		try {
+			mmOutStream.write(bytes);
+		} catch (IOException e) { }
+	}
+
+	/* Call this from the main activity to shutdown the connection */
+	public void finish() {
+		try {
+			mmSocket.close();
+			setFinished(true);
+		} catch (IOException e) { }
+	}
 
 	public boolean isFinished() {
 		return finished;
@@ -78,3 +81,4 @@ public class ConnectedThread extends Thread {
 	}
 
 }
+
