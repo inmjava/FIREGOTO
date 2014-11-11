af4df89fc713e772243be8ce6dda7896d7bafa17
rgnazar <git@astronomoamador.com.br>
2014-11-11 15:52:01 -0300

    mais controles

--- src/br/com/astronomoamador/firgoto/StatusActivity.java
+++ src/br/com/astronomoamador/firgoto/StatusActivity.java
@@ -1,7 +1,5 @@
 package br.com.astronomoamador.firgoto;
-
 import java.io.IOException;
-
 import br.com.astronomoamador.firgoto.bluetooth.ConnectedThread;
 import br.com.astronomoamador.firgoto.bluetooth.Constants;
 import android.app.Activity;
@@ -19,60 +17,40 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 import android.widget.CheckBox;
-
-
 public class StatusActivity extends Activity {
-
 	private BluetoothDevice mmDevice;
 	private ConnectedThread connectedThread;
-
-
-
 	private ToggleButton toggleNorteSulLat;
 	private EditText editTextGrauLat;
 	private EditText editTextMinLat;
 	private CheckBox CheckLat;
-
 	private ToggleButton toggleLestOestLog;
 	private EditText editTextGrauLog;
 	private EditText editTextMinLog;
 	private CheckBox CheckLog;
-
-
 	private EditText editTextHoraTime;
 	private EditText editTextMinTime;
 	private EditText editTextSegTime;
 	private CheckBox CheckTime;
-
-
 	private ToggleButton toggleUTC;
 	private EditText editTextUTCSet;
 	private int UTC = 0;
 	private CheckBox CheckUTC;
-
-
 	private TextView TextHoraUTC;
 	private TextView TextMinUTC;
 	private TextView TextSegUTC;
-
 	private TextView TextHoraLST;
 	private TextView TextMinLST;
 	private TextView TextSegLST;
-
 	private EditText editTextDia;
 	private EditText editTextMes;
 	private EditText editTextAno;
 	private CheckBox CheckData;
-
-
 	private String bufferCmd = "";
-	private String response[] = {"#","#","#","#","#","#"};
-	private String command[] = {":Gt#",":Gg#",":GG#",":GL#",":GS#",":GC#"};
+	private String response[] = {"#","#","#","#","#","#","#"};
+	private String command[] = {":Gt#",":Gg#",":GG#",":GL#",":GS#",":GC#",":Gt#"};
 	private String commandAtual = null;
 	private int icom = 0;
-
-
-
 	private final Handler mHandler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
@@ -89,7 +67,6 @@ public class StatusActivity extends Activity {
 				else
 				{
 					bufferCmd=bufferCmd+readMessage;
-
 				}
 				readBuf=null;
 				readMessage=null;
@@ -97,51 +74,35 @@ public class StatusActivity extends Activity {
 			}
 		}
 	};
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_status);
-
 		toggleNorteSulLat = (ToggleButton) findViewById(R.id.toggleButtonLat);
 		editTextGrauLat = (EditText) findViewById(R.id.editTextGrauLat);
 		editTextMinLat = (EditText) findViewById(R.id.editTextMinLat);
 		CheckLat = (CheckBox) findViewById(R.id.checkBoxLat);
-
 		toggleLestOestLog = (ToggleButton) findViewById(R.id.ToggleButtonLog);
 		editTextGrauLog = (EditText) findViewById(R.id.editTextGrauLog);
 		editTextMinLog = (EditText) findViewById(R.id.editTextMinLog);
 		CheckLog = (CheckBox) findViewById(R.id.CheckBoxLog);
-
-
 		editTextHoraTime = (EditText) findViewById(R.id.editTextHoraTime);
 		editTextMinTime = (EditText) findViewById(R.id.editTextMinTime);
 		editTextSegTime = (EditText) findViewById(R.id.editTextSegTime);
 		CheckTime = (CheckBox) findViewById(R.id.CheckBoxTime);
-
-
 		toggleUTC = (ToggleButton) findViewById(R.id.ToggleButtonUTC);
 		editTextUTCSet = (EditText) findViewById(R.id.editTextUTCSet);
 		CheckUTC = (CheckBox) findViewById(R.id.CheckBoxUTC);
-
-
 		TextHoraUTC = (TextView) findViewById(R.id.TextHoraUTC);
 		TextMinUTC = (TextView) findViewById(R.id.TextMinUTC);
 		TextSegUTC = (TextView) findViewById(R.id.TextSegUTC);
-
 		TextHoraLST = (TextView) findViewById(R.id.TextHoraLST);
 		TextMinLST = (TextView) findViewById(R.id.TextMinLST);
 		TextSegLST = (TextView) findViewById(R.id.TextSegLST);
-
 		editTextDia = (EditText) findViewById(R.id.editTextDia);
 		editTextMes = (EditText) findViewById(R.id.editTextMes);
 		editTextAno = (EditText) findViewById(R.id.editTextAno);
 		CheckData = (CheckBox) findViewById(R.id.CheckBoxData);
-
-
-
-
-
 		mmDevice = getIntent().getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 		if(mmDevice != null){
 			try {
@@ -154,12 +115,8 @@ public class StatusActivity extends Activity {
 			}
 		}
 	}
-
-
-
 	private void MandaComando()
 	{
-
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -168,7 +125,6 @@ public class StatusActivity extends Activity {
 						connectedThread.write(command[icom].getBytes());
 						commandAtual=command[icom];
 						Thread.sleep(1250);
-
 						if (icom == 0)
 						{
 							command[0]="";
@@ -193,30 +149,28 @@ public class StatusActivity extends Activity {
 						{
 							command[5]="";
 						}
+						if (icom == 6)
+						{
+							command[6]="";
+						}
 						icom++;
-						if (icom > 5)
+						if (icom > 6)
 						{
 							icom=0;
 						}
-
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-
 				}
 			}
 		}).start();
-
 	}
-
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.status, menu);
 		return true;
 	}
-
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
@@ -228,14 +182,12 @@ public class StatusActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event)
 	{
 		if ((keyCode == KeyEvent.KEYCODE_BACK))
 		{
 			if(mmDevice != null)
-
 			{
 				Intent result = new Intent();
 				result.putExtra(BluetoothDevice.EXTRA_DEVICE, mmDevice);
@@ -246,10 +198,9 @@ public class StatusActivity extends Activity {
 		}
 		return super.onKeyDown(keyCode, event);
 	}	
-
-
 	private void leResposta(String readMessage)
-	{
+	{						
+
 		int tmphh=0;
 		String strtemp;
 		if (":Gt#".equals(commandAtual)) {
@@ -262,7 +213,6 @@ public class StatusActivity extends Activity {
 			{
 				toggleLestOestLog.setChecked(true);
 			}
-
 			editTextGrauLat.setText(readMessage.subSequence(1, 3));
 			editTextMinLat.setText(readMessage.subSequence(4, 6));
 		}
@@ -298,7 +248,6 @@ public class StatusActivity extends Activity {
 			}
 			strtemp=readMessage.subSequence(0, 3).toString();
 			UTC=Integer.parseInt(strtemp);
-
 		}
 		if (":GL#".equals(commandAtual)) {
 			tmphh=tmphh+UTC;
@@ -324,25 +273,21 @@ public class StatusActivity extends Activity {
 			editTextDia.setText(readMessage.subSequence(3, 5));
 			editTextAno.setText(readMessage.subSequence(6, 8));
 		}
-		if (commandAtual.contains(":St"))
+		if (commandAtual.contains(":St")||commandAtual.contains(":Sg")||commandAtual.contains(":SL")||commandAtual.contains(":SG")||commandAtual.contains(":SC"))
 			if (readMessage.equalsIgnoreCase("0"))
 			{
-				Toast.makeText(getApplicationContext(),  R.string.atualizado, Toast.LENGTH_LONG).show();
+				Toast.makeText(getApplicationContext(), R.string.atualizado , Toast.LENGTH_LONG).show();
 			}
 			else
 			{
-				Toast.makeText(getApplicationContext(),  R.string.deu_erro, Toast.LENGTH_LONG).show();
+				Toast.makeText(getApplicationContext(), R.string.deu_erro , Toast.LENGTH_LONG).show();
 			}
-
 	}
-
 	public void atualizar(View v){
-
 		String strtmp;
 		int i;
 		if (CheckLat.isChecked())
 		{
-
 			if (!toggleNorteSulLat.isChecked())
 			{
 				strtmp=":St+";
@@ -355,15 +300,15 @@ public class StatusActivity extends Activity {
 			strtmp = strtmp + String.format("%02d", i)+"*";
 			i=Integer.parseInt(editTextMinLat.getText().toString());
 			strtmp = strtmp + String.format("%02d", i)+"#";
-			command[0] = strtmp;
-			response[0] = "0123456789";
+			command[6] = strtmp;
+			response[6] = "0123456789";
 			CheckLat.setChecked(false);
 		}
 		else
 		{
-			command[0] = ":Gt#";
+			command[6] = ":Gt#";
+			response[6] = "0123456789";
 		}
-
 		if (CheckLog.isChecked())
 		{
 			//:SgDDD*MM# 
@@ -387,17 +332,11 @@ public class StatusActivity extends Activity {
 		{
 			command[1] = ":Gg#";
 			response[1] = "#";
-
 		}
-
-
-
-
 		/*:SLHH:MM:SS# 
 Set the local Time 
 Returns: 
 0 – Invalid */
-
 		if (CheckTime.isChecked())
 		{
 			//:SLHH:MM:SS# 
@@ -416,16 +355,12 @@ Returns:
 		{
 			command[3] = ":GL#";
 			response[3] = "#";
-
 		}
-
-
 		/* :SGsHH.H# 
 Set the number of hours added to local time to yield UTC 
 Returns: 
 0 – Invalid 
-1 - Valid  */
-		
+1 - Valid */
 		if (CheckUTC.isChecked())
 		{
 			//:SGsHH# 
@@ -445,23 +380,17 @@ Returns:
 		}
 		else
 		{
-		
-
-		command[2] = ":GG#";
-		response[2] = "#";
+			command[2] = ":GG#";
+			response[2] = "#";
 		}
-
-
 		command[4] = ":GS#";
 		response[4] = "#";
-		
 		/*:SCMM/DD/YY# 
  Change Handbox Date to MM/DD/YY 
  Returns: <D><string> 
  D = ‘0’ if the date is invalid. The string is the null string. 
  D = ‘1’ for valid dates and the string is “Updating Planetary Data# #” 
  Note: For LX200GPS this is the UTC data! */
-		
 		if (CheckData.isChecked())
 		{
 			//::SCMM/DD/YY# 
@@ -480,13 +409,7 @@ Returns:
 		{
 			command[5] = ":GC#";
 			response[5] = "#";
-
 		}
-
-
-
 		icom=0;
-
 	}
-
 }
